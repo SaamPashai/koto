e02f59ba2ce4c8f1923b9693a82627a3f9e33ae2
Saam Pashai <doubleapashai@gmail.com>
2019-07-08 14:05:38 -0700

    setting up

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Jumbotron
+} from 'reactstrap';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <React.Fragment>
+            
+          </React.Fragment>
+        </Router>
+
+      </div>
+    );
+  }
+}
+
